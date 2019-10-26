dcb0ada98db29ac3f00acde3534ca7f40d4dd60d
Sen Hung Wong <alexwongsenhung@gmail.com>
2019-10-26 11:29:53 -0700

    Build basic app layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
-import { ApolloProvider } from "@apollo/react-hooks";
 import React from "react";
-import { Provider } from "react-redux";
-import { Artboard } from "./components/artboard/Artboard";
-import { Login } from "./components/auth/Login";
-import { Register } from "./components/auth/Register";
 import { SetAccessToken } from "./components/auth/SetAccessToken";
-import { AvatarBuilder } from "./components/avatar/AvatarBuilder";
-import { client } from "./graphql/client";
-import { store } from "./store";
 
 export const App: React.FC = () => {
   return (
-    <ApolloProvider client={client}>
-      <Provider store={store}>
-        <SetAccessToken />
-        <Artboard />
-        <Login />
-        <Register />
-        <AvatarBuilder />
-      </Provider>
-    </ApolloProvider>
+    <>
+      <SetAccessToken />
+      <div className="content"></div>
+    </>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,21 @@
+import { ApolloProvider } from "@apollo/react-hooks";
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import { App } from "./App";
+import { client } from "./graphql/client";
 import * as serviceWorker from "./serviceWorker";
+import { store } from "./store";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const Root = (
+  <ApolloProvider client={client}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </ApolloProvider>
+);
+
+ReactDOM.render(Root, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
