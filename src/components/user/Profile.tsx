7ae13de60e3d57bb2a3abd38ac78c74b05467dde
Sen Hung Wong <alexwongsenhung@gmail.com>
2019-11-01 21:48:56 -0700

    Mark user profile under development

--- src/components/user/Profile.tsx
+++ src/components/user/Profile.tsx
@@ -1,10 +1,14 @@
+import { message } from "antd";
 import React from "react";
 
 export const Profile: React.FC = () => {
+  const onClick = () =>
+    message.info("Sorry, user profile is under development 🚧");
+
   // TODO:
   return (
     /* eslint-disable-next-line */
-    <a>
+    <a onClick={onClick}>
       <span className="menu-dropdown">Profile</span>
     </a>
   );
