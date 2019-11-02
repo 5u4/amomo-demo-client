ef354e00ae8c5a5f2680d3cc6af19c3cf63659dd
Sen Hung Wong <alexwongsenhung@gmail.com>
2019-11-01 20:24:25 -0700

    Adjust logo style

--- src/components/control/Logo.tsx
+++ src/components/control/Logo.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Logo: React.FC = () => {
   return (
-    <div className="flex flex-col flex-wrap items-center justify-center m-4 p-0">
+    <div className="flex flex-col flex-wrap items-center justify-center p-4">
       <img alt="icon" src="/img/logo.png" style={{ width: 150 }} />
     </div>
   );
